fad3e792e81f349d28ba65530babb61a2015f53f
Sungmin Lee <ssdohammer@gmail.com>
2022-08-23 13:51:31 +0900

    qa: add validation for sample-dedup
    Signed-off-by: Sungmin Lee <sung_min.lee@samsung.com>

--- qa/tasks/deduplication.py
+++ qa/tasks/deduplication.py
@@ -5,6 +5,9 @@ import contextlib
 import logging
 import gevent
 from teuthology import misc as teuthology
+import json
+import time
+from io import StringIO
 
 
 from teuthology.orchestra import run
@@ -15,9 +18,7 @@ log = logging.getLogger(__name__)
 def task(ctx, config):
     """
     Run ceph-dedup-tool.
-
     The config should be as follows::
-
         ceph-dedup-tool:
           clients: [client list]
           op: <operation name>
@@ -26,13 +27,10 @@ def task(ctx, config):
           chunk_size: <chunk size>
           chunk_algorithm: <chunk algorithm, fixed|fastcdc>
           fingerprint_algorithm: <fingerprint algorithm, sha1|sha256|sha512>
-          object_dedup_threashold: <#% of duplicate chunks within an object to trigger object dedup>
           chunk_dedup_threashold: <the number of duplicate chunks to trigger chunk dedup>
           max_thread: <the number of threads>
           wakeup_period: <duration>
-
     For example::
-
         tasks:
         - exec:
             client.0:
@@ -45,11 +43,10 @@ def task(ctx, config):
             chunk_size: 8192
             chunk_algorithm: 'fastcdc'
             fingerprint_algorithm: 'sha1'
-            object_dedup_threshold: 15
             chunk_dedup_threshold: 5
             max_thread: 2
             wakeup_period: 20
-
+            sampling_ratio: 100
     """
     log.info('Beginning deduplication...')
     assert isinstance(config, dict), \
@@ -72,21 +69,21 @@ def task(ctx, config):
         args.extend(['--chunk-algorithm', config.get('chunk_algorithm', None)] )
     if config.get('fingerprint_algorithm', False):
         args.extend(['--fingerprint-algorithm', config.get('fingerprint_algorithm', None)] )
-    if config.get('object_dedup_threshold', False):
-        args.extend(['--object-dedup-threshold', str(config.get('object_dedup_threshold', 50))])
     if config.get('chunk_dedup_threshold', False):
-        args.extend(['--chunk-dedup-threshold', str(config.get('chunk_dedup_threshold', 5))])
+        args.extend(['--chunk-dedup-threshold', str(config.get('chunk_dedup_threshold', 2))])
     if config.get('max_thread', False):
         args.extend(['--max-thread', str(config.get('max_thread', 2))])
+    if config.get('sampling_ratio', False):
+        args.extend(['--sampling-ratio', str(config.get('sampling_ratio', 100))])
     if config.get('wakeup_period', False):
-        args.extend(['"--wakeup-period"', str(config.get('wakeup_period', 30))])
+        args.extend(['"--wakeup-period"', str(config.get('wakeup_period', 20))])
     if config.get('pool', False):
         args.extend(['--pool', config.get('pool', None)])
 
     args.extend([
         '--debug',
-        '--deamon',
-        '--iterative'])
+        '--daemon',
+        '--loop'])
 
     def thread():
         clients = ['client.{id}'.format(id=id_) for id_ in teuthology.all_roles_of_type(ctx.cluster, 'client')]
@@ -107,10 +104,94 @@ def task(ctx, config):
                 )
             tests[id_] = proc
 
+    def run_remote(args):
+        clients = ['client.{id}'.format(id=id_) for id_ in teuthology.all_roles_of_type(ctx.cluster, 'client')]
+        log.info('clients are %s' % clients)
+        manager = ctx.managers['ceph']
+        log.info("cmd: %s", args)
+        role = config.get('clients', clients)[0]
+        assert isinstance(role, str)
+        PREFIX = 'client.'
+        assert role.startswith(PREFIX)
+        id_ = role[len(PREFIX):]
+        (remote,) = ctx.cluster.only(role).remotes.keys()
+        proc = remote.run(
+            args=args.split(),
+            stdin=run.PIPE,
+            wait=True, check_status=False,
+            stdout=StringIO(),
+            )
+        if proc.exitstatus != 0:
+            return False
+        return proc.stdout.getvalue().strip()
+
+    def get_chunk_objs(chunk_pool):
+        chunk_obj_list = run_remote('rados ls -p ' + chunk_pool).split()
+        if chunk_obj_list == False:
+            return None 
+        else:
+            return chunk_obj_list
+
+    # To validate whether the sample-dedup operation works well, this function checks if 
+    #   1. sample-dedup has been started and
+    #   2. reference of chunk objects' exist in correct base pool
+    def validate():
+        log.info('start validating sample-dedup')
+        base_pool = config.get('pool', None)
+        chunk_pool = config.get('chunk_pool', None)
+        max_validation_cnt = 10
+        retry_cnt = 0
+
+        # check whether sample-dedup has been started
+        chunk_obj_list = get_chunk_objs(chunk_pool)
+        while chunk_obj_list == None and retry_cnt < max_validation_cnt:
+            # retry getting # chunk objs after 30 secs of sleep
+            time.sleep(30)
+            chunk_obj_list = get_chunk_objs(chunk_pool)
+            retry_cnt += 1
+            log.info('chunk pool empty. retry ', retry_cnt)
+        assert retry_cnt < max_validation_cnt
+
+        log.info('sample-dedup started successfully')
+
+        retry_cnt = 0
+        # validate chunk pool for max_validation_cnt times
+        while retry_cnt < max_validation_cnt:
+            for chunk_obj in chunk_obj_list:
+                # get reference list of chunk object
+                dump_str = run_remote(
+                    'ceph-dedup-tool --op dump-chunk-refs --chunk-pool '
+                    + chunk_pool + ' --object ' + chunk_obj,
+                )
+                # fail in case that reference object is not written
+                assert len(dump_str) > 0
+                log.info('{0} obj has {1} refs'
+                    .format(chunk_obj, json.loads(dump_str)['count']))
+
+                # check if chunk object's reference object exists in base-tier
+                ref_list = json.loads(dump_str)['refs']
+                for ref in ref_list:
+                    ret = run_remote('rados -p ' + base_pool + ' stat ' + ref['oid'])
+                    # check if ref exists in base pool
+                    assert len(ret)
+                    log.info('{0} obj exists in {1}'.format(ref['oid'], base_pool))
+
+            # get chunk objects for the next loop
+            chunk_obj_list = get_chunk_objs(chunk_pool)
+            retry_cnt += 1
+            time.sleep(30)
+        return True
+
+
     running = gevent.spawn(thread)
+    checker = gevent.spawn(validate)
 
     try:
         yield
     finally:
         log.info('joining ceph-dedup-tool')
         running.get()
+        check_running.get()
+        result = validate()
+        log.info('validate sample-dedup done {0}'.format(result))
+
